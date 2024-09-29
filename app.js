4a7153d47bdf84c68d61a6ededcc7ebb4f88eb44
Ethan <erday2@wisc.edu>
2024-09-29 12:20:28 -0500

    Added Color Key Modal

--- app.js
+++ app.js
@@ -15,3 +15,16 @@ signinbtn.addEventListener("click", () => {
 document.querySelector("#modalbg1").addEventListener("click", () => {
   signinmodal.classList.remove("is-active");
 });
+
+// activating color key modal
+let colorbtn = document.querySelector("#colorBtn");
+
+colorbtn.addEventListener("click", () => {
+  let colormodal = document.querySelector("#colormodal");
+  colormodal.classList.add("is-active");
+});
+
+// hide the sign in modal
+document.querySelector("#modalbg2").addEventListener("click", () => {
+  colormodal.classList.remove("is-active");
+});
